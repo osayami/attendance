0f710d590348829bb7438f8e392938dfde6f41d0
OSAYAMI <24865842+osayami@users.noreply.github.com>
2021-11-27 20:18:56 +0530

    Update Attendance.java

--- src/Attendance.java
+++ src/Attendance.java
@@ -8,30 +8,6 @@ public class Attendance {
 	
 	public static void main(String[] args) {
 		
-		/*
-		
-
-		
-		String dept="CS";
-		String year="2018-19";
-		String subject="Software engineering";
-		
-		System.out.println("Department:"+dept +"\t Year:"+year+"\tSubject:"+subject+"\n");
-		
-		
-		System.out.println(" ID \t"+"\t"+"Name"+"\t \t \t"+"Attended");
-		ArrayList<Details> list=new ArrayList<>();
-		Details st1=new Details(100,"Haany Ali",0);
-		Details st2=new Details(101,"morwan khair",0);
-		
-		list.add(st1);
-		list.add(st2);
-		check(list);
-		printDet(list);
-		Scanner s=new Scanner(System.in);
-		String college=s.next();
-		s.close();
-		*/
 		handler.start();
 		
 		
